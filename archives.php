33f49ff80c98b70ce26df2b706008576ba4fa359
Nicolas Bouliane <contact@nicolasbouliane.com>
2011-10-14 17:10:05 -0400

    Added archives template

---
+++ archives.php
@@ -0,0 +1,32 @@
+<?php
+/*
+Template Name: Archives
+*/
+get_header(); ?>
+
+<div id="container">
+	<?php the_post(); ?>
+	<div <?php post_class()?>>
+		<h1 class="title"><?php the_title()?></h1>
+		<p><?php the_date()?></p>
+		<div class="content">
+			<?php the_content()?>
+			
+			<?php get_search_form(); ?>
+			
+			<h2>Archives by Month:</h2>
+			<ul>
+				<?php wp_get_archives('type=monthly'); ?>
+			</ul>
+			
+			<h2>Archives by Subject:</h2>
+			<ul>
+				 <?php wp_list_categories(); ?>
+			</ul>
+		</div>
+
+	</div><!-- #content -->
+</div><!-- #container -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
