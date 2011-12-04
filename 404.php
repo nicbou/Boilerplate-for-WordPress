7fd4012d26e914ae1e162c1af7a65cab2b9961f5
Nicolas Bouliane <contact@nicolasbouliane.com>
2011-12-04 16:18:05 -0500

    Changed bloginfo to site_url to match the WP repository's requirements

--- 404.php
+++ 404.php
@@ -4,7 +4,7 @@
 			<h1 class="title"><?php _e('Page not found',WP_THEME_SLUG)?></h1>
 			<div class="content">
 				<p><?php _e('The page you are looking for does not exist.',WP_THEME_SLUG)?></p>
-				<p><a href="<?php bloginfo('url')?>">« <?php _e('Return to the home page',WP_THEME_SLUG)?></a></p>
+				<p><a href="<?php echo site_url()?>">« <?php _e('Return to the home page',WP_THEME_SLUG)?></a></p>
 			</div>
 		</div>
 	</div>
