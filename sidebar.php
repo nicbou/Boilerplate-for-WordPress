59bf94044984a2b72ff4717148ade985ce5e31fa
Nicolas Bouliane <contact@nicolasbouliane.com>
2012-04-17 09:19:18 -0400

    Added a few hooks

--- sidebar.php
+++ sidebar.php
@@ -1,3 +1,5 @@
 <ul id="sidebar">
+	<?php do_action('before_sidebar');?>
 	<?php dynamic_sidebar('Main sidebar'); ?>
+	<?php do_action('after_sidebar');?>
 </ul>
--- single.php
+++ single.php
@@ -2,7 +2,7 @@
 	<div id="content">
 		<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); //The Loop?>
 		<div <?php post_class()?>>
-			<h1 class="title"><?php the_title()?></h1>
+			<h1 class="title"><?php do_action('before_title'); the_title(); do_action('after_title');?></h1>
 			<p><?php the_date()?></p>
 			<div class="post-content"><?php the_content()?></div>
 			<?php if(has_tag()){the_tags( _e('Keywords','building-blocks') . ': ', ', ');}?>
