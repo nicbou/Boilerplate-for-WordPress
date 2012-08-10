5c2712088a0668f01233fac44ec2ba8334f2049a
Nicolas Bouliane <contact@nicolasbouliane.com>
2012-08-10 15:39:23 -0400

    Tiny CSS class fix

--- index.php
+++ index.php
@@ -4,12 +4,12 @@
 			<div <?php post_class()?>>
 				<h2 class="title"><a href="<?php the_permalink()?>" title="<?php the_title_attribute(); ?>" rel="bookmark"><?php do_action('before_title'); the_title(); do_action('after_title');?></a></h2>
 				<p><?php the_date()?></p>
-				<div class="page-content"><?php the_content()?></div>
+				<div class="post-content"><?php the_content()?></div>
 			</div>
 		<?php endwhile; else: //When there are no posts to list (do not confuse with 404.php)?>
 			<div class="post">
 				<h1 class="title"><?php _e('No posts found','building-blocks')?></h1>
-				<div class="page-content"><p><?php _e('There are no posts to display here.','building-blocks')?></p></div>
+				<div class="post-content"><p><?php _e('There are no posts to display here.','building-blocks')?></p></div>
 			</div>
 		<?php endif; ?>
 		
